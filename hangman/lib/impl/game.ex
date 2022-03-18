a6f839ef6a0db93339886d85eb0a8ab5d4f372b0
Raul Pereira <raul.pereira@gmail.com>
2022-03-18 19:33:06 -0300

    lesson #6: let's start coding - pattern matching game state

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,7 +1,9 @@
 defmodule Hangman.Impl.Game do
+  alias Hangman.Type
+
   @type t :: %__MODULE__{
           turns_left: integer,
-          game_state: Hangman.state(),
+          game_state: Type.state(),
           letters: list(String.t()),
           used: MapSet.t(String.t())
         }
@@ -12,11 +14,32 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )
 
-  def new_game do
+  @spec new_game() :: t
+  def new_game, do: new_game(Dictionary.random_word())
+
+  @spec new_game(String.t()) :: t
+  def new_game(word) do
     %__MODULE__{
       letters:
-        Dictionary.random_word()
+        word
         |> String.codepoints()
+    }
+  end
+
+  @spec make_move(t, String.t()) :: {t, Type.tally()}
+  def make_move(game = %{game_state: state}, _guess) when state in [:lost, :won] do
+    {game, tally(game)}
+  end
+
+  defp tally(game) do
+    %{
+      turns_left: game.turns_left,
+      game_state: game.game_state,
+      letters: [],
+      used:
+        game.used
+        |> MapSet.to_list()
+        |> Enum.sort()
     }
   end
 end
--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -17,4 +17,16 @@ defmodule HangmanImplGameTest do
     assert game.game_state == :initializing
     assert game.letters == ["w", "o", "m", "b", "a", "t"]
   end
+
+  test "state doesn't change if a game is lost or won" do
+    for state <- [:lost, :won] do
+      game =
+        Game.new_game("wombat")
+        |> Map.put(:game_state, state)
+
+      {new_game, _tally} = Game.make_move(game, "x")
+
+      assert new_game == game
+    end
+  end
 end
