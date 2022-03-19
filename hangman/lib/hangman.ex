566e31b36bfd3bb94eca45c6efd8c86db92a193f
Raul Pereira <raul.pereira@gmail.com>
2022-03-18 21:27:52 -0300

    lesson #6: let's start coding - and a bad guess

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -5,7 +5,7 @@ defmodule Hangman do
   @opaque game :: Game.t()
 
   @spec new_game() :: game
-  defdelegate new_game, to: Game
+  defdelegate new_game(), to: Game
 
   @spec make_move(game, String.t()) :: {game, Type.tally()}
   defdelegate make_move(game, guess), to: Game
--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -79,9 +79,38 @@ defmodule HangmanImplGameTest do
   test "can handle a sequence of move" do
     [
       ["a", :bad_guess, 6, ["_", "_", "_", "_", "_"], ["a"]],
-      ["a", :bad_guess, 6, ["_", "_", "_", "_", "_"], ["a"]],
+      ["a", :already_used, 6, ["_", "_", "_", "_", "_"], ["a"]],
       ["e", :good_guess, 6, ["_", "e", "_", "_", "_"], ["a", "e"]],
       ["x", :bad_guess, 5, ["_", "e", "_", "_", "_"], ["a", "e", "x"]]
+    ]
+    |> test_sequence_of_moves()
+  end
+
+  test "can handle a winning game" do
+    [
+      ["a", :bad_guess, 6, ["_", "_", "_", "_", "_"], ["a"]],
+      ["a", :already_used, 6, ["_", "_", "_", "_", "_"], ["a"]],
+      ["e", :good_guess, 6, ["_", "e", "_", "_", "_"], ["a", "e"]],
+      ["x", :bad_guess, 5, ["_", "e", "_", "_", "_"], ["a", "e", "x"]],
+      ["l", :good_guess, 5, ["_", "e", "l", "l", "_"], ["a", "e", "l", "x"]],
+      ["o", :good_guess, 5, ["_", "e", "l", "l", "o"], ["a", "e", "l", "o", "x"]],
+      ["y", :bad_guess, 4, ["_", "e", "l", "l", "o"], ["a", "e", "l", "o", "x", "y"]],
+      ["h", :won, 4, ["h", "e", "l", "l", "o"], ["a", "e", "h", "l", "o", "x", "y"]]
+    ]
+    |> test_sequence_of_moves()
+  end
+
+  test "can handle a losing game" do
+    [
+      ["a", :bad_guess, 6, ["_", "_", "_", "_", "_"], ["a"]],
+      ["b", :bad_guess, 5, ["_", "_", "_", "_", "_"], ["a", "b"]],
+      ["c", :bad_guess, 4, ["_", "_", "_", "_", "_"], ["a", "b", "c"]],
+      ["d", :bad_guess, 3, ["_", "_", "_", "_", "_"], ["a", "b", "c", "d"]],
+      ["e", :good_guess, 3, ["_", "e", "_", "_", "_"], ["a", "b", "c", "d", "e"]],
+      ["f", :bad_guess, 2, ["_", "e", "_", "_", "_"], ["a", "b", "c", "d", "e", "f"]],
+      ["g", :bad_guess, 1, ["_", "e", "_", "_", "_"], ["a", "b", "c", "d", "e", "f", "g"]],
+      ["h", :good_guess, 1, ["h", "e", "_", "_", "_"], ["a", "b", "c", "d", "e", "f", "g", "h"]],
+      ["i", :lost, 0, ["h", "e", "_", "_", "_"], ["a", "b", "c", "d", "e", "f", "g", "h", "i"]]
     ]
     |> test_sequence_of_moves()
   end
@@ -98,5 +127,7 @@ defmodule HangmanImplGameTest do
     assert tally.turns_left == turns
     assert tally.letters == letters
     assert tally.used == used
+
+    game
   end
 end
