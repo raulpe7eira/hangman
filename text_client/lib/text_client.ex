85af1ec2de72fbd3841ea9aa5746bec0582f3f67
raulpe7eira <raul.pereira@gmail.com>
2022-03-22 23:33:38 -0300

    lesson #17: distributed text clients - creating remote servers

--- text_client/lib/text_client.ex
+++ text_client/lib/text_client.ex
@@ -1,6 +1,10 @@
 defmodule TextClient do
   alias TextClient.Impl.Player
+  alias TextClient.Runtime.RemoteHangman
 
   @spec start() :: :ok
-  defdelegate start(), to: Player
+  def start() do
+    RemoteHangman.connect()
+    |> Player.start()
+  end
 end
