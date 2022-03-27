66419e0c4da293901af4e9a4b353c4004dd9c657
raulpe7eira <raul.pereira@gmail.com>
2022-03-27 09:19:13 -0300

    lesson #18: installing phoenix - installing phoenix

---
+++ b1/config/test.exs
@@ -0,0 +1,14 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :b1, B1Web.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "y8+nOqzkeJjlqTopldLh0kjfyZuilGrTuaJoQQ82nuuGuV0WJ8Tq97HTtLFQdYUA",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
