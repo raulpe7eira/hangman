261d1341613ccd21c0234188efdc0759be6039ac
Raul Pereira <raul.pereira@gmail.com>
2022-03-29 19:53:46 -0300

    lesson #19: hangman using html pages - cleanup unused servers

--- hangman/lib/runtime/server.ex
+++ hangman/lib/runtime/server.ex
@@ -2,8 +2,12 @@ defmodule Hangman.Runtime.Server do
   use GenServer
 
   alias Hangman.Impl.Game
+  alias Hangman.Runtime.Watchdog
 
   @type t :: pid
+
+  # 1 hour
+  @idle_timeout 1 * 60 * 60 * 1000
 
   ### client
 
@@ -15,17 +19,20 @@ defmodule Hangman.Runtime.Server do
 
   @impl true
   def init(_init_arg) do
-    {:ok, Game.new_game()}
+    watcher = Watchdog.start(@idle_timeout)
+    {:ok, {Game.new_game(), watcher}}
   end
 
   @impl true
-  def handle_call({:make_move, guess}, _from, game) do
+  def handle_call({:make_move, guess}, _from, {game, watcher}) do
+    Watchdog.im_alive(watcher)
     {updated_game, tally} = Game.make_move(game, guess)
-    {:reply, tally, updated_game}
+    {:reply, tally, {updated_game, watcher}}
   end
 
   @impl true
-  def handle_call({:tally}, _from, game) do
-    {:reply, Game.tally(game), game}
+  def handle_call({:tally}, _from, {game, watcher}) do
+    Watchdog.im_alive(watcher)
+    {:reply, Game.tally(game), {game, watcher}}
   end
 end
