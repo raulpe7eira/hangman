d612e632d2fc2c6c01ff3e6e1ef43c3e88f5b830
Raul Pereira <raul.pereira@gmail.com>
2022-03-31 01:06:46 -0300

    fix @remote_server

--- text_client/lib/runtime/remote_hangman.ex
+++ text_client/lib/runtime/remote_hangman.ex
@@ -1,5 +1,5 @@
 defmodule TextClient.Runtime.RemoteHangman do
-  @remote_server :hangman@dave
+  @remote_server :"hangman@DESKTOP-M3RIB7C"
 
   def connect() do
     :rpc.call(@remote_server, Hangman, :new_game, [])
